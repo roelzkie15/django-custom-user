9b4d03802e235b2a24eb6220462dde1367598195
Roel Delos Reyes <badcoder28@gmail.com>
2018-02-13 01:51:25 +0800

    Set debug to false

--- django_custom_user/settings.py
+++ django_custom_user/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ziv_z)ui#!)duad8i*wg54-=q8p6wj73ff_b)834ls9if@xo^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mysterious-sands-37277.herokuapp.com', 'localhost:8000']
 
