5959e3079818615164f9f009990b7d5ec0175a1a
Roel Delos Reyes <badcoder28@gmail.com>
2018-02-14 00:53:28 +0800

    Add database setting for heroku production

--- django_custom_user/settings.py
+++ django_custom_user/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ziv_z)ui#!)duad8i*wg54-=q8p6wj73ff_b)834ls9if@xo^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -89,12 +89,24 @@ WSGI_APPLICATION = 'django_custom_user.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd4l213pt3ptrl7',
+            'USER': 'gzqckewvyqnhqn',
+            'PASSWORD': 'mypassword',
+            'HOST': 'ec2-23-21-162-90.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
