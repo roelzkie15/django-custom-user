1debb6f4d75138ded5a825a017c210f5b729eaf5
Roel Delos Reyes <badcoder28@gmail.com>
2018-02-16 12:13:23 +0800

    Minor fix

--- django_custom_user/wsgi.py
+++ django_custom_user/wsgi.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_custom_user.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_custom_user.settings")
 application = get_wsgi_application()
